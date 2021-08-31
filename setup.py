35ce4bc8151106b4292746f7c2a8dea09fb03353
gautam-404 <gautam.anuj.1195@gmail.com>
2021-08-31 09:21:33 +0000

    Make the repo installable as a package

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class CustomCommands(setuptools.Command):
 # restriction is specified.
 REQUIRED_PACKAGES = [
     'numpy', 'tqdm', 'numpy', 'docutils', 
-    'mpi4py', 'h5py', 'wheel'
+    'mpi4py', 'h5py', 'wheel', 'amuse-framework', 'amuse-bse'
     ]
 
 
