0452b25b3e7d05164b850bcc70285c558ccd393d
gautam-404 <gautam.anuj.1195@gmail.com>
2021-08-31 09:28:08 +0000

    Make the repo installable with pip

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ class build(_build):  # pylint: disable=invalid-name
 # worker-startup log.
 CUSTOM_COMMANDS = [
     ['apt-get', 'update'],
+    ['apt-get', 'install', '-y', 'apt-transport-https'],
     ['apt-get', '--assume-yes', 'install', 'build-essential'],
     ['apt-get', '--assume-yes', 'install', 'gfortran'],
     ['apt-get', '--assume-yes', 'install', 'libopenmpi-dev'],
