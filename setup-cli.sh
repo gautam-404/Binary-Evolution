9319f57dacb566afc3df2447ed82c5155426ddab
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-03-14 15:28:19 +0530

    setup new

--- setup-cli.sh
+++ setup-cli.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-if [ "$os" = "l" ]; then
-    sudo apt-get update
-    sudo apt-get install build-essential gfortran python3-dev \
-    libopenmpi-dev openmpi-bin \
-    libgsl-dev cmake libfftw3-3 libfftw3-dev \
-    libgmp3-dev libmpfr6 libmpfr-dev \
-    libhdf5-serial-dev hdf5-tools \
-    libblas-dev liblapack-dev \
-    python3-venv python3-pip git
-fi
+sudo apt-get update
+sudo apt-get install build-essential gfortran python3-dev \
+libopenmpi-dev openmpi-bin \
+libgsl-dev cmake libfftw3-3 libfftw3-dev \
+libgmp3-dev libmpfr6 libmpfr-dev \
+libhdf5-serial-dev hdf5-tools \
+libblas-dev liblapack-dev \
+python3-venv python3-pip git
 
-python3 setup.py install
+python3 install .
