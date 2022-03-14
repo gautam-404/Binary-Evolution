57edb18d3fe70e197feedda67874626f38ea8970
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-03-14 16:23:52 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ libhdf5-serial-dev hdf5-tools \
 libblas-dev liblapack-dev \
 python3-venv python3-pip git
 
+python3 -m venv Amuse-env
+. Amuse-env/bin/activate
+alias amuse-env='. ~/virtualenvironments/Amuse-env/bin/activate'
+pip install --upgrade pip
 pip install .
